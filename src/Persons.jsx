ada9dc0fd0201dfa355d49caa1862c3ae8c90e3f
lags2022 <lguzman.58erb@outlook.com>
2023-03-07 12:48:08 -0500

    commit update

--- src/Persons.jsx
+++ src/Persons.jsx
@@ -1,12 +1,56 @@
+import { gql, useLazyQuery } from "@apollo/client";
+import { useEffect, useState } from "react";
+
+const FIND_PERSON = gql`
+  query findPersonByName($nameToSearch: String!) {
+    findPerson(name: $nameToSearch) {
+      name
+      phone
+      id
+      address {
+        street
+        city
+      }
+    }
+  }
+`;
+
 const Persons = ({ persons }) => {
+  const [getPerson, result] = useLazyQuery(FIND_PERSON);
+  //no olvidar el useLazyQuery devuelve un array con dos elementos el primero es la funcion y el segundo es el resultado
+  const [person, setPerson] = useState(null);
+
+  const showPerson = (name) => {
+    getPerson({ variables: { nameToSearch: name } });
+  };
+
+  useEffect(() => {
+    if (result.data) {
+      setPerson(result.data.findPerson); //aqui graphql es predecible y siempre va a devolver un objeto con el nombre que le pusimos en el query
+    }
+  }, [result]);
+
+  if (person) {
+    return (
+      <div>
+        <h2>{person.name}</h2>
+        <div>
+          {person.address.street}, {person.address.city}
+        </div>
+        <div>{person.phone}</div>
+        <button onClick={() => setPerson(null)}>close</button>
+      </div>
+    );
+  }
   if (persons === null) return null;
+
   return (
     <div>
       <h2>Persons</h2>
-      {persons.map((p) => (
-        <div key={p.id}>
+      {persons.map((person) => (
+        <div key={person.id} onClick={() => showPerson(person.name)}>
           <p>
-            {p.name} {p.phone}
+            {person.name} {person.phone}
           </p>
         </div>
       ))}
