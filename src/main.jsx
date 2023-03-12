d4c745080d9f1cd4ea50542da8aff92790452ae8
lags2022 <lguzman.58erb@outlook.com>
2023-03-11 19:41:16 -0500

    udpate

--- src/main.jsx
+++ src/main.jsx
@@ -5,25 +5,49 @@ import "./index.css";
 import {
   ApolloClient,
   ApolloProvider,
-  HttpLink,
+  // HttpLink,
+  createHttpLink,
   InMemoryCache,
 } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
-const getAuth = () => {
+const httpLink = createHttpLink({
+  uri: "http://localhost:4000/",
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
   const token = localStorage.getItem("phonenumber-user-token");
-  return token ? `bearer ${token}` : null;
-};
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `bearer ${token}` : null,
+    }
+  }
+});
+
+// const getAuth = () => {
+//   const token = localStorage.getItem("phonenumber-user-token");
+//   return token ? `bearer ${token}` : null;
+// };
 
 const client = new ApolloClient({
   connectToDevTools: true,
   cache: new InMemoryCache(), //aqui se guarda la informacion
-  link: new HttpLink({
-    headers: {
-      authorization: getAuth(),
-    },
-    uri: "http://localhost:4000/", //no olvidar que esto se tiene que colocar en el archivo .env variable de entorno
-  }),
+  link: authLink.concat(httpLink),
 });
+
+// const client = new ApolloClient({
+//   connectToDevTools: true,
+//   cache: new InMemoryCache(), //aqui se guarda la informacion
+//   link: new HttpLink({
+//     headers: {
+//       authorization: getAuth(),
+//     },
+//     uri: "http://localhost:4000/", //no olvidar que esto se tiene que colocar en el archivo .env variable de entorno
+//   }),
+// });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ApolloProvider client={client}>
